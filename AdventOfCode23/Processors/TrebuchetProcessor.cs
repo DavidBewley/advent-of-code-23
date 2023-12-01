d7f3a69cef7f2c45ed288e7068ffe7556da0a4f5
DavidBewley <davidmartinbewley@gmail.com>
2023-12-01 17:52:38 +0000

    01 Trebuchet

---
+++ AdventOfCode23/Processors/TrebuchetProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCode23.Processors
+{
+    public class TrebuchetProcessor
+    {
+        private readonly Dictionary<string, int> _digitConversions = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+        };
+
+        public int Process(string input)
+        {
+            var lines = input.Split('\n');
+            return lines.Sum(line => GetCombinedDigits(line, false));
+        }
+
+        public int ProcessWithWords(string input)
+        {
+            var lines = input.Split('\n');
+            return lines.Sum(line => GetCombinedDigits(line, true));
+        }
+
+        private int GetCombinedDigits(string line, bool includeWords)
+        {
+            if (string.IsNullOrEmpty(line))
+                return 0;
+
+            var first = FindDigit(line, includeWords, false);
+            var last = FindDigit(line, includeWords, true);
+
+            return int.Parse($"{first}{last}");
+        }
+
+        private int FindDigit(string line, bool includeWords, bool rightToLeftSearch)
+        {
+            var patternWithoutWordsNumbers = new Regex("(1|2|3|4|5|6|7|8|9)");
+            var patternWithoutWordsNumbersRightLeft = new Regex("(1|2|3|4|5|6|7|8|9)", RegexOptions.RightToLeft);
+            var patternWithWordsNumbers = new Regex("(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)");
+            var patternWithWordsNumbersRightLeft = new Regex("(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)", RegexOptions.RightToLeft);
+
+            Match? result;
+            if (includeWords)
+            {
+                result = rightToLeftSearch 
+                    ? patternWithWordsNumbersRightLeft.Match(line) 
+                    : patternWithWordsNumbers.Match(line);
+            }
+            else
+            {
+                result = rightToLeftSearch 
+                    ? patternWithoutWordsNumbersRightLeft.Match(line) 
+                    : patternWithoutWordsNumbers.Match(line);
+            }
+
+            if (result.Success)
+                return _digitConversions.TryGetValue(result.Value, out var digit) ? digit : int.Parse(result.Value);
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode23/Specs/01-Trebuchet.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode23.Processors;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace AdventOfCode23.Specs
+{
+    public class _01_Trebuchet
+    {
+        [Theory]
+        [InlineData("1abc2", 12)]
+        [InlineData("pqr3stu8vwx", 38)]
+        [InlineData("a1b2c3d4e5f", 15)]
+        [InlineData("treb7uchet", 77)]
+        [InlineData("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", 142)]
+        public void CorrectCalibrationIsReturned(string input, int output) 
+            => new TrebuchetProcessor().Process(input).Should().Be(output);
+
+        [Fact]
+        public void InputFileProducesResult()
+        {
+            var lines = File.ReadAllText("Inputs/01-TrebuchetInput.txt");
+            new TrebuchetProcessor().Process(lines).Should().Be(56465);
+        }
+
+        [Theory]
+        [InlineData("two1nine", 29)]
+        [InlineData("eightwothree", 83)]
+        [InlineData("abcone2threexyz", 13)]
+        [InlineData("xtwone3four", 24)]
+        [InlineData("4nineeightseven2", 42)]
+        [InlineData("zoneight234", 14)]
+        [InlineData("7pqrstsixteen", 76)]
+        [InlineData("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen", 281)]
+        public void CorrectCalibrationWithWordsIsReturned(string input, int output)
+            => new TrebuchetProcessor().ProcessWithWords(input).Should().Be(output);
+
+        [Fact]
+        public void InputFileWithWordsProducesResult()
+        {
+            var lines = File.ReadAllText("Inputs/01-TrebuchetInput.txt");
+            new TrebuchetProcessor().ProcessWithWords(lines).Should().Be(0);
+        }
+    }
+}
