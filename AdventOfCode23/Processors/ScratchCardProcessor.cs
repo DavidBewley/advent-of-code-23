0d0050c2ad0b118affa87aeb3e74f77759bb6713
DavidBewley <davidmartinbewley@gmail.com>
2023-12-04 14:14:04 +0000

    Day 4

---
+++ AdventOfCode23/Processors/ScratchCardProcessor.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode23.Processors
+{
+    public class ScratchCardProcessor
+    {
+        public int ProcessScratchCardsPart1(string input)
+        {
+            var cards = input.Split('\n');
+            return cards.Where(c => !string.IsNullOrEmpty(c)).Sum(card => new ScratchCard(card).GetWinningPoints());
+        }
+
+        public int ProcessScratchCardsPart2(string input)
+        {
+            var cards = input.Split('\n');
+            var processedCards = cards.Where(c => !string.IsNullOrEmpty(c)).Select(card => new ScratchCard(card)).Reverse().ToList();
+
+            var numberOfCardsProcessed = 0;
+            var stack = new Stack<ScratchCard>(processedCards);
+            while (stack.Any())
+            {
+                numberOfCardsProcessed++;
+                var currentCard = stack.Pop();
+                for (int i = 1; i <= currentCard.GetNumberOfMatches(); i++)
+                {
+                    stack.Push(processedCards.Single(c=>c.GameId == i + currentCard.GameId));
+                }
+            }
+
+            return numberOfCardsProcessed;
+        }
+    }
+
+    public class ScratchCard
+    {
+        private readonly Dictionary<int,int> _matchesPointValues = new()
+        {
+            {0,0},
+            {1,1},
+            {2,2},
+            {3,4},
+            {4,8},
+            {5,16},
+            {6,32},
+            {7,64},
+            {8,128},
+            {9,256},
+            {10,512},
+        };
+
+        public int GameId { get; private set; }
+        public List<int> WinningNumbers { get; private set; }
+        public List<int> FoundNumbers { get; private  set; }
+
+        public ScratchCard(string card)
+        {
+            WinningNumbers = new List<int>();
+            FoundNumbers = new List<int>();
+            GameId = int.Parse(card.Split(":")[0].Split("Card ")[1]);
+            foreach (var winningNumber in card.Split(':')[1].Split('|')[0].Split(' '))
+            {
+                if(string.IsNullOrEmpty(winningNumber))
+                    continue;
+                WinningNumbers.Add(int.Parse(winningNumber));
+            }
+            foreach (var foundNumber in card.Split('|')[1].Split(' '))
+            {
+                if (string.IsNullOrEmpty(foundNumber))
+                    continue;
+                FoundNumbers.Add(int.Parse(foundNumber));
+            }
+        }
+
+        public int GetNumberOfMatches()
+            => FoundNumbers.Distinct().Count(foundNumber => WinningNumbers.Distinct().Contains(foundNumber));
+
+        public int GetWinningPoints() 
+            => _matchesPointValues[GetNumberOfMatches()];
+    }
+}
---
+++ AdventOfCode23/Specs/04-ScratchCards.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode23.Processors;
+using FluentAssertions;
+
+namespace AdventOfCode23.Specs
+{
+    public class _04_ScratchCards
+    {
+        [Theory]
+        [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8)]
+        [InlineData("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
+        [InlineData("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
+        [InlineData("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
+        [InlineData("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
+        [InlineData("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
+        [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n", 10)]
+        public void ScratchCardWinningPointsReturnsCorrectPointValuesPart1(string input, int expectedPoints) 
+            => new ScratchCardProcessor().ProcessScratchCardsPart1(input).Should().Be(expectedPoints);
+
+        [Fact]
+        public void InputFileProducesResultPart1()
+        {
+            var lines = File.ReadAllText("Inputs/04-ScratchCardsInput.txt");
+            new ScratchCardProcessor().ProcessScratchCardsPart1(lines).Should().Be(23750);
+        }
+
+        [Theory]
+        [InlineData(
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n" + 
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n" +
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n" +
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n" +
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n" +
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            30)]
+        public void ScratchCardWinningPointsReturnsCorrectPointValuesPart2(string input, int expectedPoints)
+            => new ScratchCardProcessor().ProcessScratchCardsPart2(input).Should().Be(expectedPoints);
+
+        [Fact]
+        public void InputFileProducesResultPart2()
+        {
+            var lines = File.ReadAllText("Inputs/04-ScratchCardsInput.txt");
+            new ScratchCardProcessor().ProcessScratchCardsPart2(lines).Should().Be(13261850);
+        }
+    }
+}
