1e79ed4578a698f8f74a981d1ce6527126dbf326
DavidBewley <davidmartinbewley@gmail.com>
2023-12-06 11:29:03 +0000

    Part 1 + slow Part 2

---
+++ AdventOfCode23/Processors/BoatRaceProcessor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode23.Processors
+{
+    public class BoatRaceProcessor
+    {
+        public long GetNumberOfWaysToWin(string input)
+        {
+            var times = input.Split("Time:      ")[1].Split("\n")[0].Split(' ').Where(s => !string.IsNullOrEmpty(s)).ToList();
+            var distances = input.Split("Distance:  ")[1].Split("\n")[0].Split(' ').Where(s => !string.IsNullOrEmpty(s)).ToList();
+            long result = 1;
+
+            for (int i = 0; i < times.Count; i++)
+            {
+                result *= new BoatRace(long.Parse(times[i]), long.Parse(distances[i])).NumberOfWaysToWin;
+            }
+
+            return result;
+        }
+
+        public long GetNumberOfWaysToWinBadKerning(string input)
+        {
+            var time = input.Split("Time:      ")[1].Split("\n")[0].Replace(" ", "");
+            var distance = input.Split("Distance:  ")[1].Split("\n")[0].Replace(" ", "");
+            long result = 1;
+
+            result *= new BoatRace(long.Parse(time), long.Parse(distance)).NumberOfWaysToWin;
+
+            return result;
+        }
+    }
+
+    public class BoatRace
+    {
+        public long Time { get; set; }
+        public long RecordDistance { get; set; }
+        public long NumberOfWaysToWin { get; set; }
+
+        public BoatRace(long time, long recordDistance)
+        {
+            Time = time;
+            RecordDistance = recordDistance;
+
+            var firstWin = 0;
+
+            if (time < 1000)
+            {
+                for (int i = 0; i < Time; i++)
+                {
+                    if (CalculateIfWon(i, Time - i))
+                    {
+                        firstWin = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Time; i+=100000)
+                {
+                    if (CalculateIfWon(i, Time - i))
+                    {
+                        firstWin = i;
+                        break;
+                    }
+                }
+
+                var firstLoss = 0;
+                for (int i = firstWin; i < firstWin - 100000; i--)
+                {
+                    if (!CalculateIfWon(i, Time - i))
+                    {
+                        firstLoss = i;
+                        break;
+                    }
+                }
+
+                firstWin = firstLoss + 1;
+            }
+
+            NumberOfWaysToWin = Time - firstWin - firstWin + 1;
+
+        }
+
+        private bool CalculateIfWon(long ticksToAccelerate, long ticksToCoast)
+        {
+            var distanceTraveled = 0;
+            var speed = 0;
+            for (int i = 0; i < ticksToAccelerate; i++)
+            {
+                speed++;
+            }
+
+            for (int i = 0; i < ticksToCoast; i++)
+            {
+                distanceTraveled += speed;
+            }
+
+            return distanceTraveled > RecordDistance;
+        }
+    }
+}
---
+++ AdventOfCode23/Specs/06-BoatRace.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode23.Processors;
+using FluentAssertions;
+
+namespace AdventOfCode23.Specs
+{
+    public class _06_BoatRace
+    {
+        [Theory]
+        [InlineData(7, 9, 4)]
+        [InlineData(15, 40, 8)]
+        [InlineData(30, 200, 9)]
+        public void BoatRaceCanBeWonNumberOfWays(int raceLength, int recordDistance, int numberOfWaysToWin) 
+            => new BoatRaceProcessor().GetNumberOfWaysToWin($"Time:      {raceLength}\nDistance:  {recordDistance}").Should().Be(numberOfWaysToWin);
+
+        [Fact]
+        public void InputFileProducesResultPart1()
+        {
+            var input = File.ReadAllText("Inputs/06-BoatRace.txt");
+            new BoatRaceProcessor().GetNumberOfWaysToWin(input).Should().Be(2449062);
+        }
+
+        [Fact]
+        public void BoatRaceBadKerningCanBeWonNumberOfWays()
+            => new BoatRaceProcessor().GetNumberOfWaysToWinBadKerning($"Time:      7  15   30\nDistance:  9  40  200").Should().Be(71503);
+
+        [Fact]
+        public void InputFileProducesResultPart2()
+        {
+            var input = File.ReadAllText("Inputs/06-BoatRace.txt");
+            new BoatRaceProcessor().GetNumberOfWaysToWinBadKerning(input).Should().Be(0);
+        }
+    }
+}
