b3806f13938c5a0eba6fd75554ddea10f8360735
DavidBewley <davidmartinbewley@gmail.com>
2023-12-07 20:47:48 +0000

    07-Part-2

--- AdventOfCode23/Processors/CamelCardsProcessor.cs
+++ AdventOfCode23/Processors/CamelCardsProcessor.cs
@@ -28,6 +28,27 @@ namespace AdventOfCode23.Processors
 
             return result;
         }
+
+        public long ProcessHandsWithJoker(string input)
+        {
+            var hands = input
+                .Split('\n')
+                .Where(i => !string.IsNullOrEmpty(i)).ToList()
+                .Select(hand => new CamelCardHand(hand.Split(' ')[0], int.Parse(hand.Split(' ')[1]), true))
+                .OrderByDescending(hand => hand.HandType)
+                .ThenBy(hand => hand.GetCardValue(0))
+                .ThenBy(hand => hand.GetCardValue(1))
+                .ThenBy(hand => hand.GetCardValue(2))
+                .ThenBy(hand => hand.GetCardValue(3))
+                .ThenBy(hand => hand.GetCardValue(4))
+                .ToList();
+
+            var result = 0L;
+            for (var i = 0; i < hands.Count; i++)
+                result += (i + 1) * hands[i].Bid;
+
+            return result;
+        }
     }
 
     public class CamelCardHand
@@ -35,7 +56,8 @@ namespace AdventOfCode23.Processors
         public string Hand { get; set; }
         public int Bid { get; set; }
         public HandType HandType { get; set; }
-        public bool UsesJoker { get; set; }
+        public bool ContainsJoker { get; set; }
+        private int JokerCount { get; set; }
 
         private readonly Dictionary<char, int> _cardValues = new()
         {
@@ -54,53 +76,103 @@ namespace AdventOfCode23.Processors
             { '2', 2 },
         };
 
+        private readonly Dictionary<char, int> _cardValuesJoker = new()
+        {
+            { 'A', 14 },
+            { 'K', 13 },
+            { 'Q', 12 },
+            { 'T', 10 },
+            { '9', 9 },
+            { '8', 8 },
+            { '7', 7 },
+            { '6', 6 },
+            { '5', 5 },
+            { '4', 4 },
+            { '3', 3 },
+            { '2', 2 },
+            { 'J', 1 },
+        };
+
         public CamelCardHand(string hand, int bid, bool useJoker = false)
         {
             Hand = hand;
             Bid = bid;
-            HandType = DetermineHandType();
+            JokerCount = hand.Count(h => h == 'J');
+            ContainsJoker = useJoker && JokerCount > 0;
+            HandType = DetermineHandType(useJoker);
         }
 
-        public int GetCardValue(int positionInHand) 
-            => _cardValues[Hand[positionInHand]];
+        public int GetCardValue(int positionInHand)
+            => ContainsJoker
+                ? _cardValuesJoker[Hand[positionInHand]]
+                : _cardValues[Hand[positionInHand]];
 
-        private HandType DetermineHandType()
+        private HandType DetermineHandType(bool useJoker = false)
         {
-            if (IsFiveOfAKind())
+            if (Hand == "JJJJJ")
                 return HandType.FiveOfAKind;
-            if (IsFourOfAKind())
+            if (IsFiveOfAKind(useJoker))
+                return HandType.FiveOfAKind;
+            if (IsFourOfAKind(useJoker))
                 return HandType.FourOfAKind;
-            if (IsFullHouse())
+            if (IsFullHouse(useJoker))
                 return HandType.FullHouse;
-            if (IsThreeOfAKind())
+            if (IsThreeOfAKind(useJoker))
                 return HandType.ThreeOfAKind;
-            if (IsTwoPair())
+            if (IsTwoPair(useJoker))
                 return HandType.TwoPair;
-            if (IsOnePair())
+            if (IsOnePair(useJoker))
                 return HandType.OnePair;
 
             return HandType.HighCard;
         }
 
-        private bool IsFiveOfAKind()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 5;
+        private bool IsFiveOfAKind(bool useJoker)
+        {
+            if (useJoker && Hand.GroupBy(h => h).Where(h => h.Key != 'J').OrderByDescending(h => h.Count()).First().Count() + JokerCount == 5)
+                return true;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 5;
+        }
 
-        private bool IsFourOfAKind()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 4;
+        private bool IsFourOfAKind(bool useJoker)
+        {
+            if (useJoker && Hand.GroupBy(h => h).Where(h => h.Key != 'J').OrderByDescending(h => h.Count()).First().Count() + JokerCount == 4)
+                return true;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 4;
+        }
 
-        private bool IsFullHouse()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[0].Count() == 3
-               && Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[1].Count() == 2;
+        private bool IsFullHouse(bool useJoker)
+        {
+            if (useJoker && Hand.GroupBy(h => h).Where(h => h.Key != 'J').OrderByDescending(h => h.Count()).ToList()[0].Count()
+                         + Hand.GroupBy(h => h).Where(h => h.Key != 'J').OrderByDescending(h => h.Count()).ToList()[1].Count()
+                         + JokerCount == 5)
+                return true;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[0].Count() == 3
+                   && Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[1].Count() == 2;
+        }
 
-        private bool IsThreeOfAKind()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 3;
+        private bool IsThreeOfAKind(bool useJoker)
+        {
+            if (useJoker && Hand.GroupBy(h => h).Where(h => h.Key != 'J').OrderByDescending(h => h.Count()).First().Count() + JokerCount == 3)
+                return true;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 3;
+        }
 
-        private bool IsTwoPair()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[0].Count() == 2
-               && Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[1].Count() == 2;
+        private bool IsTwoPair(bool useJoker)
+        {
+            if (useJoker && JokerCount > 0) 
+                return false;
 
-        private bool IsOnePair()
-            => Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 2;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[0].Count() == 2
+                   && Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).ToList()[1].Count() == 2;
+        }
+
+        private bool IsOnePair(bool useJoker)
+        {
+            if (useJoker && JokerCount > 0)
+                return true;
+            return Hand.GroupBy(h => h).OrderByDescending(h => h.Count()).First().Count() == 2;
+        }
     }
 
     public enum HandType
--- AdventOfCode23/Specs/07-CamelCards.cs
+++ AdventOfCode23/Specs/07-CamelCards.cs
@@ -8,25 +8,25 @@ namespace AdventOfCode23.Specs
         private const string TestInput = "32T3K 765\nT55J5 684\nKK677 28 \nKTJJT 220\nQQQJA 483\n";
 
         [Fact]
-        public void TestInputHandsGivesCorrectResult() 
+        public void TestInputHandsGivesCorrectResult()
             => new CamelCardsProcessor().ProcessHands(TestInput).Should().Be(6440);
 
         [Theory]
-        [InlineData("AAAAA",HandType.FiveOfAKind)]
-        [InlineData("AAAAK",HandType.FourOfAKind)]
-        [InlineData("KAAAA",HandType.FourOfAKind)]
-        [InlineData("AAKAA",HandType.FourOfAKind)]
+        [InlineData("AAAAA", HandType.FiveOfAKind)]
+        [InlineData("AAAAK", HandType.FourOfAKind)]
+        [InlineData("KAAAA", HandType.FourOfAKind)]
+        [InlineData("AAKAA", HandType.FourOfAKind)]
         [InlineData("AAAKK", HandType.FullHouse)]
         [InlineData("KKAAA", HandType.FullHouse)]
         [InlineData("AAKAK", HandType.FullHouse)]
-        [InlineData("AAAKQ",HandType.ThreeOfAKind)]
-        [InlineData("KQAAA",HandType.ThreeOfAKind)]
-        [InlineData("AQAKA",HandType.ThreeOfAKind)]
-        [InlineData("AAKKQ",HandType.TwoPair)]
-        [InlineData("AQAKK",HandType.TwoPair)]
-        [InlineData("AAKQJ",HandType.OnePair)]
-        [InlineData("AKQJA",HandType.OnePair)]
-        [InlineData("AKQJT",HandType.HighCard)]
+        [InlineData("AAAKQ", HandType.ThreeOfAKind)]
+        [InlineData("KQAAA", HandType.ThreeOfAKind)]
+        [InlineData("AQAKA", HandType.ThreeOfAKind)]
+        [InlineData("AAKKQ", HandType.TwoPair)]
+        [InlineData("AQAKK", HandType.TwoPair)]
+        [InlineData("AAKQJ", HandType.OnePair)]
+        [InlineData("AKQJA", HandType.OnePair)]
+        [InlineData("AKQJT", HandType.HighCard)]
         public void HandsAreAssignedCorrectTypes(string hand, HandType expectedHandType)
             => new CamelCardHand(hand, 0).HandType.Should().Be(expectedHandType);
 
@@ -36,5 +36,46 @@ namespace AdventOfCode23.Specs
             var input = File.ReadAllText("Inputs/07-CamelCards.txt");
             new CamelCardsProcessor().ProcessHands(input).Should().Be(248453531);
         }
+
+        [Theory]
+        [InlineData("JJJJJ", HandType.FiveOfAKind)]
+        [InlineData("AAAAA", HandType.FiveOfAKind)]
+        [InlineData("JAAAA", HandType.FiveOfAKind)]
+        [InlineData("AAJAA", HandType.FiveOfAKind)]
+        [InlineData("AJJJJ", HandType.FiveOfAKind)]
+        [InlineData("AAJJJ", HandType.FiveOfAKind)]
+        [InlineData("AAAJJ", HandType.FiveOfAKind)]
+        [InlineData("AAAAJ", HandType.FiveOfAKind)]
+        [InlineData("AAAAK", HandType.FourOfAKind)]
+        [InlineData("AAAJK", HandType.FourOfAKind)]
+        [InlineData("JAAAK", HandType.FourOfAKind)]
+        [InlineData("KKAAA", HandType.FullHouse)]
+        [InlineData("AAKAK", HandType.FullHouse)]
+        [InlineData("AAAKQ", HandType.ThreeOfAKind)]
+        [InlineData("KQAAA", HandType.ThreeOfAKind)]
+        [InlineData("AQAKA", HandType.ThreeOfAKind)]
+        [InlineData("AAJKQ", HandType.ThreeOfAKind)]
+        [InlineData("AJJKQ", HandType.ThreeOfAKind)]
+        [InlineData("AAKKQ", HandType.TwoPair)]
+        [InlineData("AQAKK", HandType.TwoPair)]
+        [InlineData("AJKQT", HandType.OnePair)]
+        [InlineData("A7KQJ", HandType.OnePair)]
+        [InlineData("AKQJT", HandType.OnePair)]
+        public void HandsAreAssignedCorrectTypesJoker(string hand, HandType expectedHandType)
+            => new CamelCardHand(hand, 0, true).HandType.Should().Be(expectedHandType);
+
+        [Fact]
+        public void TestInputHandsGivesCorrectResultWithJokers()
+            => new CamelCardsProcessor().ProcessHandsWithJoker(TestInput).Should().Be(5905);
+
+        [Fact]
+        public void InputFileProducesResultPart2()
+        {
+            var input = File.ReadAllText("Inputs/07-CamelCards.txt");
+            new CamelCardsProcessor().ProcessHandsWithJoker(input).Should().Be(248781813);
+        }
+
+        //249083748
+        //249083748
     }
 }
