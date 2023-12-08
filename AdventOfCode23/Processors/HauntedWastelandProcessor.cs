af81a6ab9b52ee5a3601c835dd1be7399b7f0136
DavidBewley <davidmartinbewley@gmail.com>
2023-12-08 19:18:52 +0000

    08-Part-1

---
+++ AdventOfCode23/Processors/HauntedWastelandProcessor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode23.Processors
+{
+    public class HauntedWastelandProcessor
+    {
+        private readonly string _pattern;
+        private readonly Dictionary<string, (string left, string right)> _mapDirections;
+
+        public HauntedWastelandProcessor(string input)
+        {
+            _pattern = input.Split("\n\n")[0].Trim();
+            _mapDirections = new Dictionary<string, (string left, string right)>();
+            var directions = input.Split("\n\n")[1].Split("\n").Where(l => !string.IsNullOrEmpty(l)).ToList();
+            foreach (var direction in directions)
+            {
+                var node = direction.Split(" = ")[0];
+                var left = direction.Split("(")[1].Split(",")[0].Trim();
+                var right = direction.Split(",")[1].Split(")")[0].Trim();
+                _mapDirections.Add(node, (left, right));
+            }
+        }
+
+        public int CalculateNumberOfTurnsToReachDestination()
+        {
+            var numberOfTurnsToReachDestination = 0;
+            var destinationFound = false;
+            var currentLocation = "AAA";
+            var indexInPattern = 0;
+
+            while (!destinationFound)
+            {
+                currentLocation = GetNextDirection(indexInPattern) == 'L' 
+                    ? _mapDirections[currentLocation].left 
+                    : _mapDirections[currentLocation].right;
+                numberOfTurnsToReachDestination++;
+                indexInPattern = GetNextIndexForPattern(indexInPattern);
+                if (currentLocation == "ZZZ")
+                    destinationFound = true;
+            }
+
+            return numberOfTurnsToReachDestination;
+        }
+
+        private char GetNextDirection(int indexInPattern) 
+            => _pattern[indexInPattern];
+
+        private int GetNextIndexForPattern(int indexInPattern)
+        {
+            indexInPattern++;
+            return indexInPattern == _pattern.Length 
+                ? 0 
+                : indexInPattern;
+        }
+    }
+}
---
+++ AdventOfCode23/Specs/08-HauntedWasteland.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode23.Processors;
+using FluentAssertions;
+
+namespace AdventOfCode23.Specs
+{
+    public class _08_HauntedWasteland
+    {
+        private const string _exampleOne = "RL\n\nAAA = (BBB, CCC)\nBBB = (DDD, EEE)\nCCC = (ZZZ, GGG)\nDDD = (DDD, DDD)\nEEE = (EEE, EEE)\nGGG = (GGG, GGG)\nZZZ = (ZZZ, ZZZ)";
+        private const string _exampleTwo = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)";
+
+        [Theory]
+        [InlineData(_exampleOne,2)]
+        [InlineData(_exampleTwo, 6)]
+        public void ExampleInputReachesDestinationInExpectedMoves(string input, int expectedMoves) 
+            => new HauntedWastelandProcessor(input).CalculateNumberOfTurnsToReachDestination().Should().Be(expectedMoves);
+
+        [Fact]
+        public void InputFileProducesResultPart1()
+        {
+            var input = File.ReadAllText("Inputs/08-HauntedWasteland.txt");
+            new HauntedWastelandProcessor(input).CalculateNumberOfTurnsToReachDestination().Should().Be(13771);
+        }
+
+    }
+}
